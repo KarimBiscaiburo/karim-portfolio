8011b4ec0c35ce6cd7b090bda5380390dafe1014
Karim Biscaiburo <kbiscaiburo@gmail.com>
2022-07-13 19:32:41 -0300

    Apartado Contact

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -4,23 +4,23 @@ import "../sass/layout/_contacto.scss";
 function Contacto() {
     return(
         <form action="https://formsubmit.co/kbiscaiburo@gmail.com" name="contacto" method="POST" className="formulario">
-            <ul className="formulario__lista">
-                <li className="label">
-                    <label className="label__info" htmlFor="nombre">Nombre</label>
-                    <input className="input__nombre" type="text" name="nombre" />
-                </li>
-                <li className="label">
-                    <label className="label__info" htmlFor="email">Email:</label>
-                    <input className="input__email" type="email" name="email"  />
-                </li>
-                <li className="label">
-                    <label className="label__info" htmlFor="mensaje">Mensaje:</label>
-                    <textarea className="input__mensaje" name="mensaje"></textarea>
-                </li>
-                <li className="input-enviar">
-                    <input className="btn-enviar" type="submit" />
-                </li>
-            </ul>  
+            <fieldset className="formulario__campos">
+                <div className="campo">
+                    <label className="info" htmlFor="nombre">Nombre</label>
+                    <input id="formNombre" className="input__nombre" type="text" name="nombre" />
+                </div>
+                <div className="campo">
+                    <label className="info" htmlFor="email">Email:</label>
+                    <input id="formEmail" className="input__email" type="email" name="email"  />
+                </div>
+                <div className="campo">
+                    <label className="info" htmlFor="mensaje">Mensaje:</label>
+                    <textarea id="formMensage" className="input__mensaje" name="mensaje"></textarea>
+                </div>
+                <div className="campo input-enviar">
+                    <button className="btn-enviar" type="submit">Enviar</button>
+                </div>
+            </fieldset>  
         </form>
     );
 } 
