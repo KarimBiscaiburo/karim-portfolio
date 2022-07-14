deb58508955b4e3cd9c8c627094ec38bed2e1cb8
Karim Biscaiburo <kbiscaiburo@gmail.com>
2022-07-13 21:28:13 -0300

    Validacion contacto

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -2,9 +2,59 @@ import React from "react";
 import "../sass/layout/_contacto.scss";
 
 function Contacto() {
+    //En el formulario agregamos el atributo "onsubmit"
+    async function enviarFormulario(e) {
+        try {
+            const resultado = await validarFormulario();
+            if (!resultado){
+                e.preventDefault();
+                mostrarError();
+            }
+
+        } catch (error) {
+            console.log(error);
+        } 
+    }
+
+    function validarFormulario () {
+        const nombre = document.querySelector("#formNombre").value;
+        const email = document.querySelector("#formEmail").value;
+        const mensaje = document.querySelector("#formMensaje").value;
+
+        if (nombre !== "" && email !== "" && mensaje !== "") {
+            return true;
+        } else{
+            return false;
+        }
+    }
+
+    function mostrarError() {
+        //Selecciono elementos
+        const fieldset = document.querySelector("#fieldset");
+        const btnEnviar = document.querySelector("#enviar");
+
+        //Creo el mensaje que se va a agregar
+        const mensaje = document.createElement("div");
+        mensaje.classList.add("campo", "mensaje");
+
+        const texto = document.createElement("p");
+        texto.textContent = "Todos los campos son obligatorios";
+
+        //Agrego
+        mensaje.appendChild(texto);
+        fieldset.appendChild(mensaje);
+        btnEnviar.disabled = true;
+        
+        //Elimino
+        setTimeout(() => {
+            mensaje.remove();
+            btnEnviar.disabled = false;
+        }, 2500);
+    }
+
     return(
-        <form action="https://formsubmit.co/kbiscaiburo@gmail.com" name="contacto" method="POST" className="formulario">
-            <fieldset className="formulario__campos">
+        <form onSubmit={enviarFormulario} action="https://formsubmit.co/kbiscaiburo@gmail.com" name="contacto" method="POST" className="formulario">
+            <fieldset id="fieldset" className="formulario__campos">
                 <div className="campo">
                     <label className="info" htmlFor="nombre">Nombre</label>
                     <input id="formNombre" className="input__nombre" type="text" name="nombre" />
@@ -15,10 +65,10 @@ function Contacto() {
                 </div>
                 <div className="campo">
                     <label className="info" htmlFor="mensaje">Mensaje:</label>
-                    <textarea id="formMensage" className="input__mensaje" name="mensaje"></textarea>
+                    <textarea id="formMensaje" className="input__mensaje" name="mensaje"></textarea>
                 </div>
                 <div className="campo input-enviar">
-                    <button className="btn-enviar" type="submit">Enviar</button>
+                    <button id="enviar" className="btn enviar" type="submit">Enviar</button>
                 </div>
             </fieldset>  
         </form>
